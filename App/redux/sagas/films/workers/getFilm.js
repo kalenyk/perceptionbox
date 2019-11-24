949353b135999d20e6b59298d7cd370deba49b1e
Pavel Kalenyk <pasha.k.2909@gmail.com>
2019-11-24 22:52:09 +0200

    push unsaved file

--- App/redux/sagas/films/workers/getFilm.js
+++ App/redux/sagas/films/workers/getFilm.js
@@ -1,16 +1,4 @@
 import {getFilmSucceed,getFilmFailure} from '../../../actions/films/getFilms';
-import {put, call} from 'redux-saga/effects';
 import getDataHelper from '../../helpers/getDataHelper';
 
 export default getDataHelper(getFilmSucceed,getFilmFailure,'https://swapi.co/api/films/')
-// export default function*({payload}){
-//     try {
-//         const response = yield call(fetch, `https://swapi.co/api/films/${payload}`);
-//         const responseBody = yield response.json();
-//         yield put(getFilmSucceed({[payload]:{...responseBody}}))
-//     }
-//     catch(error) {
-//         console.log('error',error)
-//         yield put(getFilmFailure(error))
-//     }
-// }
